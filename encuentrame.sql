4046f7f155690046d3f00b077149024b1c7ee275
Mauricio Bustamante <mauricio@bustamanteortega.com>
2012-12-01 13:29:44 -0300

    Cambiado charset a UTF8. Agregada tabla EstadoNino. Cambiadas Ñ por N. Arreglada relacion entre ciudad y pais

--- encuentrame.sql
+++ encuentrame.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.5.2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-12-2012 a las 15:23:15
--- Versión del servidor: 5.5.27
--- Versión de PHP: 5.4.7
+-- Tiempo de generación: 01-12-2012 a las 13:24:34
+-- Versión del servidor: 5.5.25a
+-- Versión de PHP: 5.3.15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ciudad` (
-  `nombre` varchar(255) NOT NULL,
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_bin NOT NULL,
   `longitud` float DEFAULT NULL,
   `latitud` float DEFAULT NULL,
+  `paisidpais` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKciudad870850` (`paisidpais`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `EstadoNiño`
+--
+
+CREATE TABLE IF NOT EXISTS `EstadoNiño` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -41,14 +55,16 @@ CREATE TABLE IF NOT EXISTS `ciudad` (
 --
 
 CREATE TABLE IF NOT EXISTS `niño` (
-  `comentario` varchar(255) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `fechanacimiento` date NOT NULL,
   `idniño` int(10) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(255) COLLATE utf8_bin NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_bin NOT NULL,
+  `fechanacimiento` date NOT NULL,
   `ciudadid` int(10) NOT NULL,
+  `EstadoNiñoid` int(10) NOT NULL,
   PRIMARY KEY (`idniño`),
-  KEY `FKniño352757` (`ciudadid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `FKniño352757` (`ciudadid`),
+  KEY `FKniño943030` (`EstadoNiñoid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -58,11 +74,9 @@ CREATE TABLE IF NOT EXISTS `niño` (
 
 CREATE TABLE IF NOT EXISTS `pais` (
   `idpais` int(10) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `ciudadid` int(10) DEFAULT NULL,
-  PRIMARY KEY (`idpais`),
-  KEY `FKpais400439` (`ciudadid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `nombre` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idpais`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -71,12 +85,12 @@ CREATE TABLE IF NOT EXISTS `pais` (
 --
 
 CREATE TABLE IF NOT EXISTS `rasgoniño` (
-  `valor` varchar(255) DEFAULT NULL,
+  `valor` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `niñoidniño` int(10) NOT NULL,
   `tiporasgoid` int(10) NOT NULL,
   KEY `FKrasgoniño696666` (`niñoidniño`),
   KEY `FKrasgoniño989012` (`tiporasgoid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -85,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `rasgoniño` (
 --
 
 CREATE TABLE IF NOT EXISTS `reporte` (
-  `comentario` varchar(255) NOT NULL,
+  `comentario` varchar(255) COLLATE utf8_bin NOT NULL,
   `fecha` date NOT NULL,
   `idreporte` int(10) NOT NULL AUTO_INCREMENT,
   `niñoidniño` int(10) DEFAULT NULL,
@@ -93,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `reporte` (
   PRIMARY KEY (`idreporte`),
   KEY `FKreporte969038` (`niñoidniño`),
   KEY `FKreporte400054` (`ciudadid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -102,27 +116,28 @@ CREATE TABLE IF NOT EXISTS `reporte` (
 --
 
 CREATE TABLE IF NOT EXISTS `tiporasgo` (
-  `nombrerasgo` varchar(255) NOT NULL,
+  `nombrerasgo` varchar(255) COLLATE utf8_bin NOT NULL,
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(255) NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD CONSTRAINT `FKciudad870850` FOREIGN KEY (`paisidpais`) REFERENCES `pais` (`idpais`);
+
+--
 -- Filtros para la tabla `niño`
 --
 ALTER TABLE `niño`
+  ADD CONSTRAINT `FKniño943030` FOREIGN KEY (`EstadoNiñoid`) REFERENCES `EstadoNiño` (`id`),
   ADD CONSTRAINT `FKniño352757` FOREIGN KEY (`ciudadid`) REFERENCES `ciudad` (`id`);
-
---
--- Filtros para la tabla `pais`
---
-ALTER TABLE `pais`
-  ADD CONSTRAINT `FKpais400439` FOREIGN KEY (`ciudadid`) REFERENCES `ciudad` (`id`);
 
 --
 -- Filtros para la tabla `rasgoniño`
